67eb025df4def1e94e162e187e35bdd690c93cb3
GuitarCYQ <1159543426@qq.com>
2020-09-20 15:18:27 +0800

    重置密码

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -23,7 +23,7 @@ class SessionsController extends Controller
     {
         $credentials = $this->validate($request, [
             'email' => 'required|email|max:255',
-            'password' => 'required'
+            'password' => 'requird'
         ]);
 
         if (Auth::attempt($credentials, $request->has('remember'))){
