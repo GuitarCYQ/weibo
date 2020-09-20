6b56ef357470e48f1e03cbe2cbe2bf72c9f7a234
GuitarCYQ <1159543426@qq.com>
2020-09-20 16:20:28 +0800

    用户微博列表

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -23,7 +23,7 @@ class SessionsController extends Controller
     {
         $credentials = $this->validate($request, [
             'email' => 'required|email|max:255',
-            'password' => 'requird'
+            'password' => 'required'
         ]);
 
         if (Auth::attempt($credentials, $request->has('remember'))){
